de588e8dd0b734f3e8eff1a0773d1f8dfdf0ea24
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-09-24 18:12:14 +0200

    [UPD] nominate rotte menu header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,17 +15,16 @@
                 <div class="col-12 d-flex py-5 ">
                     <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
                     <ul class="list-unstyled d-flex">
-                        {{-- <li class="px-2 mt-2"><a class="text-decoration-none text-dark" href="{{ route('homepage') }}">Home</a></li> --}}
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark" href="{{ route('characters') }}">Characters</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Comics</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Movie</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">TV</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Games</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Collectibles</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Videos</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Fans</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">News</a></li>
-                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Shop</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'characters' ? 'active' : ''}}" href="{{ route('characters') }}">Characters</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'comics' ? 'active' : ''}}"" href="{{ route('comics')}}">Comics</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'movies' ? 'active' : ''}}" href="{{ route('movies') }}">Movies</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'tv' ? 'active' : ''}}" href="{{ route('tv') }}">TV</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'games' ? 'active' : ''}}" href="{{ route('games') }}">Games</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'collectibles' ? 'active' : ''}}" href="{{ route('collectibles') }}">Collectibles</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'videos' ? 'active' : ''}}" href="{{ route('videos') }}">Videos</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'fans' ? 'active' : ''}}" href="{{ route('fans') }}">Fans</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'news' ? 'active' : ''}}" href="{{ route('news') }}">News</a></li>
+                        <li class="px-2 mt-2"><a class="nav-link {{ Route::currentRouteName() === 'shop' ? 'active' : ''}}" href="{{ route('shop') }}">Shop</a></li>
                     </ul>
                 </div>
             </div>
