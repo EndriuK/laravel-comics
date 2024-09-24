022c3829b6c2bef407112daeaa401ef6aed228ea
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-09-24 17:15:55 +0200

    [UPD] aggiunto list-menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>DC COMICS</h1>
+                <h1 class="text-center">DC COMICS</h1>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DC Comics</title>
+
+    @vite('resources/js/app.js')
 </head>
 <body>
     <header>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 d-flex py-5 ">
                     <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                    <ul class="list-unstyled d-flex">
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark" href="{{ route('homepage') }}">Home</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Characters</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Comics</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Movie</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">TV</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Games</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Collectibles</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Videos</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Fans</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">News</a></li>
+                        <li class="px-2 mt-2"><a class="text-decoration-none text-dark">Shop</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
